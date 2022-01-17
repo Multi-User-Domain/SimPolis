a4050713826844f6667b00d48dccfdbb9dd6eae3
Calum Mackervoy <c.mackervoy@gmail.com>
2022-01-17 19:04:37 +0100

    animating character movement

--- characters/Player/Player.gd
+++ characters/Player/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 
+onready var animation_player = $AnimationPlayer
 export var speed = 400  # How fast the player will move (pixels/sec).
 export var character_name = ""
 var target_coords
@@ -19,9 +20,17 @@ func _physics_process(delta):
 		if (target_coords - get_position()).length() > 10:
 			velocity = (target_coords - get_position()).normalized() * speed
 			move_and_slide(velocity, Vector2(0, -1))
+			if velocity.x > 0:
+				animation_player.play("RunRight")
+			else:
+				animation_player.play("RunLeft")
 		else:
 			set_position(target_coords)
 			target_coords = null
+			if velocity.x > 0:
+				animation_player.play("IdleRight")
+			else:
+				animation_player.play("IdleLeft")
 
 func _on_KinematicBody2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
