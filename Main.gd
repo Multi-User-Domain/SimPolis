796cfdb1e778f0dc32e05faf710dd0368fbce0ee
Calum Mackervoy <c.mackervoy@gmail.com>
2022-01-17 17:55:58 +0100

    renamed player variables to character

--- Main.gd
+++ Main.gd
@@ -1,20 +1,20 @@
 extends Node2D
 
-var selected_player
+var selected_character
 
 func _ready():
-	$PlayerOne.position = Vector2(63, 84)
-	$PlayerTwo.position = Vector2(263, 84)
-	_connect_character($PlayerOne)
-	_connect_character($PlayerTwo)
+	$Themistocles.position = Vector2(63, 84)
+	$Pericles.position = Vector2(263, 84)
+	_connect_character($Themistocles)
+	_connect_character($Pericles)
 
 func _connect_character(character):
-	character.connect("player_selected", self, "select_player", [character])
+	character.connect("player_selected", self, "select_character", [character])
 
-func select_player(player):
-	selected_player = player
+func select_character(character):
+	selected_character = character
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		if selected_player != null and event.button_index == BUTTON_RIGHT:
-			selected_player.target_coords = event.position
+		if selected_character != null and event.button_index == BUTTON_RIGHT:
+			selected_character.target_coords = event.position
