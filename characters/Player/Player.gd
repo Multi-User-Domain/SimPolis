2ead8c03340d31db85e90bef9202920c357abd38
Calum Mackervoy <c.mackervoy@gmail.com>
2022-01-17 13:26:35 +0100

    player movement

--- characters/Player/Player.gd
+++ characters/Player/Player.gd
@@ -10,6 +10,15 @@ var screen_size  # Size of the game window.
 func _ready():
 	screen_size = get_viewport_rect().size
 
+func _physics_process(delta):
+	if target_coords:
+		if (target_coords - get_position()).length() > 10:			
+			velocity = (target_coords - get_position()).normalized() * speed
+			move_and_slide(velocity, Vector2(0, -1))
+		else:
+			set_position(target_coords)
+			target_coords = null
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == BUTTON_RIGHT:
