55eb61c0bfbca9d28feaab86e7d6cdd7d7301176
Calum Mackervoy <c.mackervoy@gmail.com>
2022-01-24 14:13:08 +0100

    card animation on hover

--- objects/cards/CardDisplay.gd
+++ objects/cards/CardDisplay.gd
@@ -1,11 +1,23 @@
-extends Sprite
+extends Node2D
 
+var init_scale
+var init_position
+var focus_position
+export var grow_factor: float = 1.5
 
 func _ready():
-	set_visible(false)
+	init_scale = get_scale()
+	init_position = get_position()
+	focus_position = init_position + Vector2(0, -100)
 
 func _on_Card_mouse_entered():
-	set_visible(true)
+	$BackgroundGlow.set_visible(true)
+	$Tween.interpolate_property(self, "position", init_position, focus_position, .5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property(self, "scale", init_scale, init_scale * grow_factor, .5,Tween.TRANS_QUAD,Tween.EASE_IN_OUT)
+	$Tween.start()
 
 func _on_Card_mouse_exited():
-	set_visible(false)
+	$BackgroundGlow.set_visible(false)
+	$Tween.interpolate_property(self, "position", focus_position, init_position, .5, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property(self, "scale", init_scale * grow_factor, init_scale, .5,Tween.TRANS_QUAD,Tween.EASE_IN_OUT)
+	$Tween.start()
