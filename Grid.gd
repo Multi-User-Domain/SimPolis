85969e3c470a9c844efd67825f385555ca63dbf9
Calum Mackervoy <c.mackervoy@gmail.com>
2022-02-04 17:12:35 +0100

    fix issue where grid tile changed on move

--- Grid.gd
+++ Grid.gd
@@ -14,9 +14,6 @@ func _ready():
 		inhabitants.append([])
 		inhabitants[x].resize(grid_height)
 
-func _get_node_cell_type(node):
-	return node.get_cell_type() if node.has_method("get_cell_type") else 1
-
 func request_move_direction(actor, direction: Vector2):
 	#
 	# :return: False if not possible to move, the new grid location if it is
@@ -25,12 +22,9 @@ func request_move_direction(actor, direction: Vector2):
 	direction = direction.normalized()
 	var cell_target = cell_start + direction
 	
-	var cell_target_type = get_cellv(cell_target)
-	match cell_target_type:
-		-1:
-			return move_to_cell(actor, cell_target)
-		1:
-			return false
+	if get_node_in_cell(cell_target != null):
+		return move_to_cell(actor, cell_target)
+	return false
 
 func move_to_cell(node, world_target: Vector2):
 	var node_cell = world_to_map(node.position)
@@ -44,14 +38,12 @@ func move_to_cell(node, world_target: Vector2):
 func empty_cell(cell: Vector2):
 	assert(cell.x >= 0 and cell.x < grid_width and cell.y >= 0 and cell.y < grid_height, "Cell index out of bounds!")
 	
-	set_cellv(cell, -1)
 	inhabitants[cell.x][cell.y] = null
 
 func place_in_cell(node, cell: Vector2, set_physical_position: bool = true):
 	assert(cell.x >= 0 and cell.x < grid_width and cell.y >= 0 and cell.y < grid_height, "Cell index out of bounds!")
 	
 	# store references to the node's position
-	set_cellv(cell, _get_node_cell_type(node))
 	inhabitants[cell.x][cell.y] = node
 	
 	# set the node's physical position on screen
@@ -67,7 +59,7 @@ func can_move_to_coords(target_coords: Vector2):
 	return can_move_to_cell(world_to_map(target_coords))
 
 func can_move_to_cell(target_cell: Vector2):
-	return get_cellv(target_cell) <= 0
+	return get_node_in_cell(target_cell) == null
 
 func world_to_cell_centre(vector_target: Vector2):
 	return map_to_cell_centre(world_to_map(vector_target))
