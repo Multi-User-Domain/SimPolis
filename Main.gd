fa46ac77db27b7e289f87b5f13a668943b3fbea5
Calum Mackervoy <c.mackervoy@gmail.com>
2022-02-05 20:00:01 +0100

    able to spawn foxes by acting on card

--- Main.gd
+++ Main.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var place_item_prompt = get_node("ItemPlacePrompt")
 onready var grid = get_node("Grid")
 var selected_character: Character
+var selected_card: Node2D
 
 # will be set to the hovered card by child (for detecting if a card has been selected
 var mouse_hovering_over_card: Node2D = null
@@ -51,27 +52,39 @@ func _handle_interaction(target_node, target_cell):
 		selected_character.set_target_coords(grid.move_to_cell(selected_character, grid.map_to_world(target_cell)))
 		selected_character.connect("destination_arrived", self, "complete_interaction", [selected_character, target_node])
 
+func clear_selected_card():
+	selected_card = null
+	place_item_prompt.clear()
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		
 		# we have received a select command (left click)
 		if mouse_hovering_over_card != null and event.button_index == BUTTON_LEFT:
 			var new_fox = character_scene.instance()
+			selected_card = mouse_hovering_over_card
 			place_item_prompt.set_new_item(new_fox)
 		
 		# we have received an action command (right click)
-		if selected_character != null and event.button_index == BUTTON_RIGHT:
+		elif event.button_index == BUTTON_RIGHT:
 			
-			# if the cell is empty then move there
-			var target_cell: Vector2 = grid.world_to_map(event.position)
-			if grid.can_move_to_cell(target_cell):
-				selected_character.set_target_coords(grid.move_to_cell(selected_character, event.position))
-			# there is something in the cell
-			else:
-				# can I interact with it?
-				var target_node = grid.get_node_in_cell(target_cell)
-				if target_node.has_method("interact"):
-					_handle_interaction(target_node, target_cell)
+			# a card is active
+			if selected_card != null:
+				# perform the action and deselect if successful
+				selected_card.act(grid.world_to_map(event.position))
+			
+			# no card active, move the selected character
+			elif selected_character != null:
+				# if the cell is empty then move there
+				var target_cell: Vector2 = grid.world_to_map(event.position)
+				if grid.can_move_to_cell(target_cell):
+					selected_character.set_target_coords(grid.move_to_cell(selected_character, event.position))
+				# there is something in the cell
+				else:
+					# can I interact with it?
+					var target_node = grid.get_node_in_cell(target_cell)
+					if target_node.has_method("interact"):
+						_handle_interaction(target_node, target_cell)
 
 func complete_interaction(agent_node, target_node):
 	target_node.interact(agent_node)
--- objects/cards/CardDisplay.gd
+++ objects/cards/CardDisplay.gd
@@ -4,12 +4,23 @@ onready var game = get_tree().current_scene
 var init_scale
 var init_position
 var focus_position
-export var grow_factor: float = 1.5
+export(float) var grow_factor = 1.5
+export(Globals.CARD_TYPE) var card_type = Globals.CARD_TYPE.PLACE
 
 func _ready():
 	init_scale = get_scale()
 	init_position = get_position()
 	focus_position = init_position + Vector2(0, -100)
+
+func act(map_position: Vector2):
+	# for now the card just places its object in the cell
+	var success = game.grid.place_in_cell(game.place_item_prompt, map_position, false)
+	
+	if success:
+		var spawned_item = game.place_item_prompt.extract_item()
+		spawned_item.set_position(game.grid.map_to_cell_centre(map_position))
+		game.grid.add_child(spawned_item)
+		game.clear_selected_card()
 
 func _on_Card_mouse_entered():
 	game.mouse_hovering_over_card = self
