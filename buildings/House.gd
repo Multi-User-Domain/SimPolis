60878855a1da1d8919f0f7c6d6725618309baae4
Calum Mackervoy <c.mackervoy@gmail.com>
2022-06-12 16:12:39 +0300

    update: House overrides Building save

--- buildings/House.gd
+++ buildings/House.gd
@@ -3,3 +3,8 @@ extends "res://buildings/Building.gd"
 
 func _ready():
 	pass
+
+func save(world_position=null):
+	var save_data = .save(world_position)
+	save_data["@type"] = Globals.MUD_BUILDING.HOUSE
+	return save_data
